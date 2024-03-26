373eee663ba41321fcddd65500cc1464f3f1ea2b
santhoshmlops <santhoshmlops@gmail.com>
2024-03-26 22:52:46 +0530

    Update : Setup File

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Create a new directory called "model" within the repository
+mkdir model
+cd model
+
+# Clone the Base model and embedding repository from Hugging Face.
+git clone https://huggingface.co/santhoshmlops/Skai-gemma-2b-it-SFT-GGUF
+git clone https://huggingface.co/BAAI/bge-large-en-v1.5
+
+cd ../
+
+# Download Gemma - 2B Model using ollama for local inferencing
+ollama run gemma:2b
