5bf830e94cc658b7b1c2b574cb7b57bda8dc679e
Santhosh Kumar <133121635+santhoshmlops@users.noreply.github.com>
2024-03-29 09:44:06 +0530

    Update : setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,3 @@ cd model
 # Clone the Base model and embedding repository from Hugging Face.
 git clone https://huggingface.co/santhoshmlops/Skai-gemma-2b-it-SFT-GGUF
 git clone https://huggingface.co/BAAI/bge-large-en-v1.5
-
-cd ../
-
-# Download Gemma - 2B Model using ollama for local inferencing
-ollama run gemma:2b
